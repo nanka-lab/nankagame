826062a19d3b907ed2049423007f9be1612019de
ryuki0007 <ryuki.ishida.9y@stu.hosei.ac.jp>
2024-09-05 10:15:11 +0900

    フォントのパス取得とちょっとスネークに変えた

--- nankagame.py
+++ nankagame.py
@@ -1,5 +1,11 @@
-import pygame, sys, datetime
+import pygame as pg
+import sys
+import datetime
+import os
 from pygame.locals import *
+
+this_dir = os.path.dirname(__file__) #このファイルのディレクトリ名を取得
+font_path = os.path.join(this_dir, "assets", "fonts", "ヒラギノ角ゴシック W1.ttc") #フォントファイルのパスを取得
 
 class Player():
     def __init__(self):
@@ -28,11 +34,11 @@ class Player():
 class Stage():
     def __init__(self):
         #各変数の初期値設定
-        self.gridSize = 40 #1マスの長さ
+        self.grid_size = 40 #1マスの長さ
         self.height = 9  #ステージの行数
         self.width = 11 #ステージの列数
-        self.heightBlank = 150 #縦の空白部分
-        self.widthBlank = 200 #横の空白部分
+        self.height_blank = 150 #縦の空白部分
+        self.width_blank = 200 #横の空白部分
         self.stage = [] #ステージを管理する配列
         #0が空白マス，1が壁，2がイベントマス, "player"がプレイヤーの色
         self.colors = {0:(0, 0, 0), 1:(115, 66, 41), 2:(233, 168, 38), "player":(0, 0, 255)} #順番にblack、brown、orange、blue
@@ -66,7 +72,7 @@ class Game():
         self.player = Player() #Playerのインスタンスを作成
         self.state = 0 #0がタイトル画面，1がゲーム画面，2が目標リスト画面
         self.running = True #メインループ回すとき，この変数使うのが定番らしい．あまり気にしなくていい
-        self.clock = pygame.time.Clock() #時間オブジェクトの作成．fpsを管理するのに使う
+        self.clock = pg.time.Clock() #時間オブジェクトの作成．fpsを管理するのに使う
         self.time = 0 #管理している時間．0はまだ管理している時間がない状態．
         self.timeLimit = 360 #制限時間
 
@@ -79,12 +85,12 @@ class Game():
         self.brown = (115, 66, 41)
         self.orange = (233, 168, 38)
         self.gray = (127, 127, 127)
-        self.height = (self.stage.height + 2) * self.stage.gridSize + self.stage.heightBlank + (self.stage.heightBlank - 60) #ウィンドウの高さ．式キモいけど見逃して．
-        self.width = (self.stage.width + 2) * self.stage.gridSize + self.stage.widthBlank * 2               #ウィンドウの長さ．同上
+        self.height = (self.stage.height + 2) * self.stage.grid_size + self.stage.height_blank + (self.stage.height_blank - 60) #ウィンドウの高さ．式キモいけど見逃して．
+        self.width = (self.stage.width + 2) * self.stage.grid_size + self.stage.width_blank * 2               #ウィンドウの長さ．同上
 
-        pygame.init() #おまじない．気にしなくていい
-        pygame.display.set_caption("なんかげーむ") #ウィンドウの名前を指定できます
-        self.screen = pygame.display.set_mode((self.width, self.height)) #ウィンドウ作成
+        pg.init() #おまじない．気にしなくていい
+        pg.display.set_caption("なんかげーむ") #ウィンドウの名前を指定できます
+        self.screen = pg.display.set_mode((self.width, self.height)) #ウィンドウ作成
         self.stage.createStage() #ステージを管理する配列を作成
     
     def run(self):
@@ -93,14 +99,14 @@ class Game():
                 self.screen.fill(self.black) #背景を全部黒にして
 
                 #タイトル作ります
-                titleFont = pygame.font.Font("ヒラギノ角ゴシック W1.ttc", 74) #タイトルを描画するフォントを設定
+                titleFont = pg.font.Font(font_path, 74) #タイトルを描画するフォントを設定
                 titleText = titleFont.render("なんかげーむ", True, self.white)
                 titleTextRect = titleText.get_rect(center = (self.width // 2, self.height // 2 - 150))
                 self.screen.blit(titleText, titleTextRect)
 
                 #スタートボタン作ります
-                startButton = pygame.draw.rect(self.screen, self.green, (self.width // 2 - 100, self.height // 2, 200, 50))
-                startButtonFont = pygame.font.Font("ヒラギノ角ゴシック W1.ttc", 28) #ボタンのフォント設定
+                startButton = pg.draw.rect(self.screen, self.green, (self.width // 2 - 100, self.height // 2, 200, 50))
+                startButtonFont = pg.font.Font(font_path, 28) #ボタンのフォント設定
                 startButtonText = startButtonFont.render("げーむすたーと", True, self.black)
                 startButtonTextRect = startButtonText.get_rect(center = (self.width // 2, self.height // 2 + 25))
                 self.screen.blit(startButtonText, startButtonTextRect)
@@ -108,42 +114,42 @@ class Game():
             elif self.state == 1: #ゲーム画面の描写
                 self.screen.fill(self.gray) #背景を全部灰色にする
                 #ステージ部分の背景を白にする．これも式キモいけど許して
-                pygame.draw.rect(self.screen, self.white, (self.stage.widthBlank, self.stage.heightBlank, (self.stage.width + 2) * self.stage.gridSize, (self.stage.height + 2) * self.stage.gridSize))
+                pg.draw.rect(self.screen, self.white, (self.stage.width_blank, self.stage.height_blank, (self.stage.width + 2) * self.stage.grid_size, (self.stage.height + 2) * self.stage.grid_size))
 
                 #ステージを描画する
-                stageFont = pygame.font.Font("ヒラギノ角ゴシック W1.ttc", 25) #座標を描画するフォントの設定
+                stageFont = pg.font.Font(font_path, 25) #座標を描画するフォントの設定
 
                 for y in range(self.stage.height + 2):
                     for x in range(self.stage.width + 2):
                         state = self.stage.stage[y][x] #マスの状態を管理する変数
 
                         if state == 0: #マスが空白なら枠線作る
-                            pygame.draw.rect(self.screen, self.stage.colors[state],
-                                             (x * self.stage.gridSize + self.stage.widthBlank, y * self.stage.gridSize + self.stage.heightBlank, self.stage.gridSize, self.stage.gridSize), 1)
+                            pg.draw.rect(self.screen, self.stage.colors[state],
+                                             (x * self.stage.grid_size + self.stage.width_blank, y * self.stage.grid_size + self.stage.height_blank, self.stage.grid_size, self.stage.grid_size), 1)
                         elif state == 1: #マスが壁なら壁を描写する
-                            pygame.draw.rect(self.screen, self.stage.colors[state],
-                                             (x * self.stage.gridSize + self.stage.widthBlank, y * self.stage.gridSize + self.stage.heightBlank, self.stage.gridSize, self.stage.gridSize))
+                            pg.draw.rect(self.screen, self.stage.colors[state],
+                                             (x * self.stage.grid_size + self.stage.width_blank, y * self.stage.grid_size + self.stage.height_blank, self.stage.grid_size, self.stage.grid_size))
                         elif state == 2: #マスがイベントマスならそれも描く描くしかじか
-                            pygame.draw.rect(self.screen, self.stage.colors[state],
-                                             (x * self.stage.gridSize + self.stage.widthBlank, y * self.stage.gridSize + self.stage.heightBlank, self.stage.gridSize, self.stage.gridSize))
+                            pg.draw.rect(self.screen, self.stage.colors[state],
+                                             (x * self.stage.grid_size + self.stage.width_blank, y * self.stage.grid_size + self.stage.height_blank, self.stage.grid_size, self.stage.grid_size))
 
                         #x軸の座標の描画．式キモめ
                         if y == 0 and x >= 1 and x <= self.stage.width:
                             xText = stageFont.render(f"{x}", True, self.white)
                             xTextRect = xText.get_rect(center = (
-                                (x * self.stage.gridSize + self.stage.widthBlank) + self.stage.gridSize // 2 - 1, (y * self.stage.gridSize + self.stage.heightBlank) + self.stage.gridSize // 2))
+                                (x * self.stage.grid_size + self.stage.width_blank) + self.stage.grid_size // 2 - 1, (y * self.stage.grid_size + self.stage.height_blank) + self.stage.grid_size // 2))
                             self.screen.blit(xText, xTextRect)
 
                         #y軸の座標の描画．式キモめ2
                         if x == 0 and y >= 1 and y <= self.stage.height:
                             yText = stageFont.render(f"{chr(64+y)}", True, self.white)
                             yTextRect = yText.get_rect(center = (
-                                (x * self.stage.gridSize + self.stage.widthBlank) + self.stage.gridSize // 2, (y * self.stage.gridSize + self.stage.heightBlank) + self.stage.gridSize // 2))
+                                (x * self.stage.grid_size + self.stage.width_blank) + self.stage.grid_size // 2, (y * self.stage.grid_size + self.stage.height_blank) + self.stage.grid_size // 2))
                             self.screen.blit(yText, yTextRect)
 
                 #プレイヤーを描画する．これも式キモいね，ごめんね，頑張ってね
-                pygame.draw.circle(self.screen, self.stage.colors["player"],
-                                   (self.player.x * self.stage.gridSize + self.stage.widthBlank + self.stage.gridSize // 2, self.player.y * self.stage.gridSize + self.stage.heightBlank + self.stage.gridSize // 2), 15)
+                pg.draw.circle(self.screen, self.stage.colors["player"],
+                                   (self.player.x * self.stage.grid_size + self.stage.width_blank + self.stage.grid_size // 2, self.player.y * self.stage.grid_size + self.stage.height_blank + self.stage.grid_size // 2), 15)
 
                 #時間管理
                 now = datetime.datetime.now() #現在時刻の取得
@@ -156,14 +162,14 @@ class Game():
                     self.state = 3 #ゲームオーバー画面に移行
 
                 #メニューの描画
-                pygame.draw.rect(self.screen, self.white, (0, 0, self.width, 60)) #メニューの背景を白色に
-                menuFont = pygame.font.Font("ヒラギノ角ゴシック W1.ttc", 40) #メニューのフォントの設定
+                pg.draw.rect(self.screen, self.white, (0, 0, self.width, 60)) #メニューの背景を白色に
+                menuFont = pg.font.Font(font_path, 40) #メニューのフォントの設定
                 menuText = menuFont.render(f"ステージ1：1F   フロア数：5   制限時間：{self.timeLimit}s", True, self.black)
                 self.screen.blit(menuText, (22, 10)) #いい感じのところに配置
 
                 #目標確認ボタン作ります
-                goalButton = pygame.draw.rect(self.screen, self.black, (self.width - 90, 70, 80, 40))
-                goalButtonFont = pygame.font.Font("ヒラギノ角ゴシック W1.ttc", 28) #ボタンのフォント設定
+                goalButton = pg.draw.rect(self.screen, self.black, (self.width - 90, 70, 80, 40))
+                goalButtonFont = pg.font.Font(font_path, 28) #ボタンのフォント設定
                 goalButtonText = goalButtonFont.render("目標", True, self.white)
                 goalButtonTextRect = goalButtonText.get_rect(center = (self.width - 90 + 80 // 2, 70 + 40 // 2))
                 self.screen.blit(goalButtonText, goalButtonTextRect)
@@ -172,32 +178,32 @@ class Game():
                 self.screen.fill(self.gray) #背景を全部灰色にする
 
                 #目標確認リスト作ります
-                goalList = pygame.draw.rect(self.screen, self.white, (50, 50, self.width - 100, self.height - 100))
-                goalListFont = pygame.font.Font("ヒラギノ角ゴシック W1.ttc", 50) #フォント設定
+                goalList = pg.draw.rect(self.screen, self.white, (50, 50, self.width - 100, self.height - 100))
+                goalListFont = pg.font.Font(font_path, 50) #フォント設定
                 goalListText = goalListFont.render("ここでデータベース班の出番だ！", True, self.black)
                 goalListTextRect = goalListText.get_rect(center = (self.width // 2, self.height // 2))
                 self.screen.blit(goalListText, goalListTextRect)
 
                 #戻るボタンを作ります
-                backButton = pygame.draw.rect(self.screen, self.black, (10, 10, 60, 30))
-                backButtonFont = pygame.font.Font("ヒラギノ角ゴシック W1.ttc", 25) #フォント設定
+                backButton = pg.draw.rect(self.screen, self.black, (10, 10, 60, 30))
+                backButtonFont = pg.font.Font(font_path, 25) #フォント設定
                 backButtonText = backButtonFont.render("戻る", True, self.white)
                 backButtonTextRect = backButtonText.get_rect(center = (10 + 60 // 2, 10 + 30 // 2))
                 self.screen.blit(backButtonText, backButtonTextRect)
 
             elif self.state == 3: #ゲームオーバー画面の描写
                 self.screen.fill(self.black) #背景を真っ黒にする
-                gameoverFont = pygame.font.Font("ヒラギノ角ゴシック W1.ttc", 50) #フォント設定
+                gameoverFont = pg.font.Font(font_path, 50) #フォント設定
                 gameoverText = gameoverFont.render("げーむおーばー", True, self.red)
                 gameoverTextRect = gameoverText.get_rect(center = (self.width // 2, self.height // 2))
                 self.screen.blit(gameoverText, gameoverTextRect)
 
-            pygame.display.update() #今までの変更を全部反映させる
+            pg.display.update() #今までの変更を全部反映させる
 
             #イベント処理
-            for event in pygame.event.get():
+            for event in pg.event.get():
                 if event.type == QUIT: #ウィンドウが閉じられたら終了
-                    pygame.quit()
+                    pg.quit()
                     sys.exit()
                 elif event.type == KEYDOWN: #移動キー押されたら動こう
                     if self.state == 1: #ゲーム画面ならプレイヤー移動
