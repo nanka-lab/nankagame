eade687ad6d7046775da0486c2f915c8d04a18f9
ryuki0007 <ryuki.ishida.9y@stu.hosei.ac.jp>
2024-09-13 21:19:54 +0900

    直しました

--- test/test_stage1.py
+++ test/test_stage1.py
@@ -2,4 +2,4 @@ import pytest
 from src.data.maps import stage1
 
 def test_practice():
-    assert stage1.PLAYER_INIT["1F"] == (2,2), "(2,2)じゃないよ"
+    assert stage1.PLAYER_INIT == (1,1), "(1,1)じゃないよ"
