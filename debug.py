eb0d00639342bce36016b3cf95e33a6ca5d776ee
ryuki0007 <ryuki.ishida.9y@stu.hosei.ac.jp>
2024-09-22 16:49:16 +0900

    ミニゲーム完成　これ制限時間内にクリアしたら神

---
+++ debug.py
@@ -0,0 +1,3 @@
+from src.minigames.rendagame import rendagame_main
+
+rendagame_main.main()
--- src/minigames/rendagame/rendagame_main.py
+++ src/minigames/rendagame/rendagame_main.py
@@ -2,11 +2,18 @@ import pygame as pg
 from pygame.locals import *
 from . import classes as cl
 from . import consts as c
+from . import setup
 
 def main():
+    pg.init()
+    pg.event.set_allowed([pg.KEYDOWN, pg.KEYUP, pg.QUIT])
+    pg.display.set_caption(c.GAME_TITLE)
+    SCREEN = pg.display.set_mode(c.SCREEN_SIZE)
     game = cl.Control()
     state_dict = {c.START: cl.Start(),
                   c.PLAY: cl.Play(),
+                  c.COUNT_DOWN: cl.Count_down(),
                   c.RESULT: cl.Result()}
     game.setup_status(state_dict, c.START)
-    game.main()
+    result = game.main()
+    return result
