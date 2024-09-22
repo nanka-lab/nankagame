9dac34c90590a58a01637a02c4e7f35c8c7ae77f
ryuki0007 <ryuki.ishida.9y@stu.hosei.ac.jp>
2024-09-22 16:59:26 +0900

    test修正

--- test/test_stage1.py
+++ test/test_stage1.py
@@ -1,9 +1,3 @@
 import pytest
 #from src.data.maps import stage1
 from src.minigames.rendagame import rendagame_main
-
-def test_renda():
-    rendagame_main.main()
-
-#def test_practice():
-    assert stage1.PLAYER_INIT == (1,1), "(1,1)じゃないよ"
